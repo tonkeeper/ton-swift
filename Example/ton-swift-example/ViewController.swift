a82625906a882a8261c62f9901ce2cb8375ec2d0
zloysergunya <zloysergunya@mail.ru>
2023-03-07 21:42:38 +0700

    add example app

---
+++ Example/ton-swift-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ton-swift-example
+//
+//  Created by Sergey Kotov on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
