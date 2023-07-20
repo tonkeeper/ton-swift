ac7fedd565db8f02262eb7845aa1c5fdf050255b
Kirill Kirilenko <godemodegame@icloud.com>
2023-07-20 17:38:47 +0400

    remove useless extension in PublicKey

--- Source/TonSwift/Keys/PublicKey.swift
+++ Source/TonSwift/Keys/PublicKey.swift
@@ -7,27 +7,25 @@
 
 import Foundation
 
-public struct PublicKey: Key {
+public struct PublicKey: Key, CellCodable {
     public let data: Data
     
     public init(data: Data) {
         self.data = data
     }
-}
 
-extension PublicKey: CellCodable {
+    private static let publicKeyLength = 32
+
+    // MARK: CellCodable
+
     public func storeTo(builder: Builder) throws {
         try builder.store(data: data)
     }
     
     public static func loadFrom(slice: Slice) throws -> PublicKey {
         try slice.tryLoad { s in
-            let data = try s.loadBytes(.publicKeyLength)
+            let data = try s.loadBytes(publicKeyLength)
             return PublicKey(data: data)
         }
     }
-}
-
-private extension Int {
-    static let publicKeyLength = 32
 }
