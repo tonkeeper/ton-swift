20c17ee92b85bcdd4a78c2eb0467fcd6de5f7698
Grigory Serebryanyy <grishamsc@gmail.com>
2023-10-16 15:23:20 +0200

    Util
    - make Data.sha256 public

--- Source/TonSwift/Util/SHA256.swift
+++ Source/TonSwift/Util/SHA256.swift
@@ -2,7 +2,7 @@ import CommonCrypto
 import Foundation
 
 extension Data {
-    func sha256() -> Self {
+    public func sha256() -> Self {
         var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
         withUnsafeBytes {
             _ = CC_SHA256($0.baseAddress, CC_LONG(count), &hash)
