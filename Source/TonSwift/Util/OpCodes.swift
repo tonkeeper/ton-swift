915094091eb0d9d6fe0ac64655df5c19f14a53a1
Grigory Serebryanyy <grishamsc@gmail.com>
2024-12-05 17:11:27 +0100

    OpCodes
    - add STONFI_SWAP_V2 opcode

--- Source/TonSwift/Util/OpCodes.swift
+++ Source/TonSwift/Util/OpCodes.swift
@@ -5,6 +5,7 @@ public enum OpCodes {
   public static var JETTON_TRANSFER: Int32 = 0xf8a7ea5
   public static var NFT_TRANSFER: Int32 = 0x5fcc3d14
   public static var STONFI_SWAP: Int32 = 0x25938561
+  public static var STONFI_SWAP_V2: Int32 = 0x6664de2a
   public static var CHANGE_DNS_RECORD: Int32 = 0x4eb1f0f9
   public static var LIQUID_TF_DEPOSIT: Int32 = 0x47d54391
   public static var LIQUID_TF_BURN: Int32 = 0x595f07bc
